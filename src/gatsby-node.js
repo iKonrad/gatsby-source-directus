ccdad0ab790dd5a9df71a184fb8da98aa1033037
Josh Kramer <josh.kramer@measuresforjustice.org>
2018-10-11 09:52:49 -0400

    Fetch all files first, before the table data

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -51,44 +51,6 @@ exports.sourceNodes = async ({ boundActionCreators, getNode, store, cache, creat
     // Initialize the Fetcher class with API key and URL
     const fetcher = new Fetcher(_apiKey, _url, _version, _requestParams, _fileRequestParams);
 
-    console.log(`gatsby-source-directus`.cyan, 'Fetching Directus tables data...');
-
-    // Fetch all the tables with data from Directus in a raw format
-    const allTablesData = await fetcher.getAllTablesData();
-
-    let items = [];
-    console.log(`gatsby-source-directus`.blue, 'success'.green, `Fetched`, allTablesData.length.toString().yellow, `tables from Directus.`)
-
-    for (let tableData of allTablesData) {
-        const tableNode = TableNode(tableData);
-        await createNode(tableNode);
-        let tableItems = await fetcher.getAllItemsForTable(tableData.name);
-        console.log(`gatsby-source-directus`.blue, 'success'.green, `Fetched`, tableItems.length.toString().cyan, `items for `, tableData.name.cyan, ` table...`)
-
-        // Get the name for this node type
-        let name = getNodeTypeNameForTable(tableData.name, nameExceptions);
-        console.log(`gatsby-source-directus`.blue,  'info'.cyan, `Generating Directus${name} node type...`);
-
-        // We're creating a separate Item Type for every table
-        let ItemNode = createTableItemFactory(name);
-
-        if (tableItems && tableItems.length > 0) {
-            // Get all the items for the table above and create a gatsby node for it
-            for (let tableItemData of tableItems) {
-                // Create a Table Item node based on the API response
-                const tableItemNode = ItemNode(tableItemData, {
-                    parent: tableNode.id,
-                });
-
-                // Pass it to Gatsby to create a node
-                await createNode(tableItemNode);
-            }
-            console.log(`gatsby-source-directus`.blue, `success`.green, `Directus${name} node generated`);
-        } else {
-            console.log(`gatsby-source-directus`.blue, `warning`.yellow, `${tableData.name} table has no rows. Skipping...`);
-        }
-    }
-
     console.log(`gatsby-source-directus`.cyan, 'Fetching Directus files data...');
 
     const allFilesData = await fetcher.getAllFiles();
@@ -127,6 +89,44 @@ exports.sourceNodes = async ({ boundActionCreators, getNode, store, cache, creat
         console.log(`gatsby-source-directus`.blue, 'success'.green, `Downloaded all`, filesDownloaded.toString().yellow, `files from Directus.`);
     } else {
         console.log(`gatsby-source-directus`.blue, `warning`.yellow, `skipped`, (filesDownloaded - allFilesData.length).toString().yellow, 'files from downloading');
+    }
+
+    console.log(`gatsby-source-directus`.cyan, 'Fetching Directus tables data...');
+
+    // Fetch all the tables with data from Directus in a raw format
+    const allTablesData = await fetcher.getAllTablesData();
+
+    let items = [];
+    console.log(`gatsby-source-directus`.blue, 'success'.green, `Fetched`, allTablesData.length.toString().yellow, `tables from Directus.`)
+
+    for (let tableData of allTablesData) {
+        const tableNode = TableNode(tableData);
+        await createNode(tableNode);
+        let tableItems = await fetcher.getAllItemsForTable(tableData.name);
+        console.log(`gatsby-source-directus`.blue, 'success'.green, `Fetched`, tableItems.length.toString().cyan, `items for `, tableData.name.cyan, ` table...`)
+
+        // Get the name for this node type
+        let name = getNodeTypeNameForTable(tableData.name, nameExceptions);
+        console.log(`gatsby-source-directus`.blue,  'info'.cyan, `Generating Directus${name} node type...`);
+
+        // We're creating a separate Item Type for every table
+        let ItemNode = createTableItemFactory(name);
+
+        if (tableItems && tableItems.length > 0) {
+            // Get all the items for the table above and create a gatsby node for it
+            for (let tableItemData of tableItems) {
+                // Create a Table Item node based on the API response
+                const tableItemNode = ItemNode(tableItemData, {
+                    parent: tableNode.id,
+                });
+
+                // Pass it to Gatsby to create a node
+                await createNode(tableItemNode);
+            }
+            console.log(`gatsby-source-directus`.blue, `success`.green, `Directus${name} node generated`);
+        } else {
+            console.log(`gatsby-source-directus`.blue, `warning`.yellow, `${tableData.name} table has no rows. Skipping...`);
+        }
     }
 
     console.log("AFTER");
